b7faccfd548702fad7f15ebb5014c4ef6501dd3a
KZellweger <kevin.zellweger@students.fhnw.ch>
2020-09-22 14:19:58 +0200

    add add observer demo project

--- ColorPicker/src/main/java/sample/Observer.java
+++ ColorPicker/src/main/java/sample/Observer.java
@@ -5,6 +5,7 @@ package sample;
  * @Date 15.09.20
  */
 public abstract class Observer {
+    //TODO: make color Subject parameter of update method, then you can use an interface
     protected ColorSubject colorSubject;
     public abstract void update();
 }
